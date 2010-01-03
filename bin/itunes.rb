5a37b1889a06a607ad2c432c1679ff2960f3edab
V Sreekanth <sreeix@gmail.com>
2010-01-03 21:13:57 +0530

    Indenting and removing options

--- bin/itunes.rb
+++ bin/itunes.rb
@@ -1,5 +1,6 @@
-require 'itunes_library'
+require File.join(File.dirname(__FILE__),"..","lib",'itunes_library')
 lib=ItunesLibrary.new("data/music.xml")
 puts lib.number_of_songs
 puts lib.search :first, :name=>"Constant Motion"
 puts lib.search :all, :artist=>"Dream Theater"
+puts lib.search :all, :artist_regex=>/Dream/
--- lib/itunes_library.rb
+++ lib/itunes_library.rb
@@ -1,23 +1,23 @@
 require 'rubygems'
 require 'nokogiri'
-require 'song'
+require File.join(File.dirname(__FILE__),'song')
 class ItunesLibrary
   attr_reader :songs
   def initialize(path)
     puts "reading the library"
     doc = Nokogiri::XML(IO.read(path))
     @songs = doc.root.xpath("//plist/dict/dict/dict").collect{|xml| Song.new(xml)}
-    
+
   end
 
   def number_of_songs
     @songs.size
   end
-  
+
   def search(scope=:first, opt={})
     case scope
       when :first then @songs.detect{|song| song.match? opt}
-      when :all   then @songs.select{|song| puts song;song.match? opt}
+      when :all   then @songs.select{|song| song.match? opt}
     end
   end
 end
