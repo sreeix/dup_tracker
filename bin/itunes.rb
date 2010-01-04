4980e468c04c74b40abcdabe58f944da336bec77
V Sreekanth <sreeix@gmail.com>
2010-01-04 16:54:36 +0530

    Adding the example of regex matches

--- bin/itunes.rb
+++ bin/itunes.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__),"..","lib",'itunes_library')
+require 'uri'
 lib=ItunesLibrary.new("data/music.xml")
 puts lib.number_of_songs
 puts lib.search :first, :name=>"Constant Motion"
 #puts lib.search :all, :artist=>"Dream Theater"
-puts lib.search :all, :name=>Proc.new{|name| name=~ /^Dream/}
+puts lib.search :all, :location=>Proc.new{|str| str.match URI::escape("Tech Talks")}
